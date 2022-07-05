79a7d964125d71064a769e33f0940e73acf6b88a
Henrique Haas Dal Castel <77808747+HenriqueCastel@users.noreply.github.com>
2022-07-05 18:35:17 -0300

    Add files via upload

---
+++ CursoJavascriptV2/funcoes/exercicio_imc.js
@@ -0,0 +1,45 @@
+/*
+Muito abaixo do peso - 16 a 16,9 kg/m2
+Abaixo do peso - 17 a 18,4 kg/m2
+Peso normal - 18,5 a 24,9 kg/m2
+Acima do peso - 25 a 29,9 kg/m2
+Obesidade Grau I - 30 a 34,9 kg/m2
+Obesidade Grau II - 35 a 40 kg/m2
+Obesidade Grau III - maior que 40 kg/m2
+*/
+
+function calcularIMC(peso, altura) {
+    if (peso === undefined || altura === undefined) {
+        throw Error("need two parameters: weight and height")
+    }
+    return peso / (altura * altura)
+}
+
+function classificaIMC(imc) {
+    if (imc >= 16 && imc <= 16.9) {
+        return 'Muito abaixo do peso';
+    } 
+    if (imc >= 17 && imc <= 18.4) {
+        return 'Abaixo do peso';
+    } 
+    if (imc >= 18.5 && imc <= 24.9) {
+        return 'Peso normal';
+    } 
+    if (imc >= 25 && imc <= 29.9) {
+        return 'Acima do peso';
+    } 
+    if (imc >= 30 && imc <= 34.9) {
+        return 'Obesidade Grau I';
+    } 
+    if (imc >= 35 && imc <= 40) {
+        return 'Obesidade Grau II';
+    } 
+    if (imc > 40) {
+        return 'Obesidade Grau III';
+    }
+}
+
+let imc = calcularIMC(80, 1.65)
+console.log(imc)
+console.log(classificaIMC(imc))
+
